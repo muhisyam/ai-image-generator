b8556e382dcd8bd15e46dfe40224b23f1152dee1
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-02-07 20:17:23 +0700

    add create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
+
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-2[500px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis alias neque praesentium. Delectus, vitae at.</p>
+      </div>
+    </section>
   )
 }
 
