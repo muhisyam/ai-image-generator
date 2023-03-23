aaf91f13b3488d3f69f411c0a70eb6e4e1d71560
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-03-23 14:43:36 +0700

    add card image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,6 +15,33 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null)
   const [searchText, setSearchText] = useState('')
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if(response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (err) {
+        alert(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPosts();
+  }, [])
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -50,7 +77,7 @@ const Home = () => {
           />
         ) : (
           <RenderCards
-            data={[]}
+            data={allPosts}
             title="No posts found"
           />
         )}
